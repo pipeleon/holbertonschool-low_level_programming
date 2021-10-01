0cbf52f07e709e7588852abc02ed9f4dfc16864e
pipeleon <oflg94@gmail.com>
2021-09-30 22:06:37 -0700

    Task 100

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
  */
 int main(void)
 {
-	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(STDOUT_FILENO, x, strlen(x));
 	/* return an int */
 	return (1);
 }
