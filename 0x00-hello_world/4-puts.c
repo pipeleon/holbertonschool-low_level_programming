1661fdd77675a8ecf40b379fda1bf3d88b95ca06
pipeleon <oflg94@gmail.com>
2021-09-30 20:40:30 -0700

    Task 4 v3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  */
 int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  puts(str);
-  return (0);
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(str);
+	return (0);
 }
