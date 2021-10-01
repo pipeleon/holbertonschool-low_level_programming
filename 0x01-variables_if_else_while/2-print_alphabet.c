2e80e29177c13fe1f29f8c0c96f860fbd801727b
pipeleon <oflg94@gmail.com>
2021-10-01 12:23:15 -0700

    Task1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* Task 1 0x01 Variables, if, else, while  */
+
+/**
+ * main - Print lower case letter
+ * Description: Print lower case leters
+ * Return: 0
+ */
+
+void main(void)
+{
+	int a;
+
+	for(a = 97; a <=122; a++){
+		printf("%c", a);
+	}
+}
