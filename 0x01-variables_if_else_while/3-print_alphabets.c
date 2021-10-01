f1834ab1f7ac572b995138b16a90de7d68f39814
pipeleon <oflg94@gmail.com>
2021-10-01 12:35:50 -0700

    Task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* Task 1 0x01 Variables, if, else, while  */
+
+/**
+ * main - Print lower and upper case letter
+ * Description: Print lower and upper case leters
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a <= 122; a++)
+		putchar(a);
+
+	for (b = 65; b <= 90; b++)
+		putchar(b);
+
+	return (0);
+}
