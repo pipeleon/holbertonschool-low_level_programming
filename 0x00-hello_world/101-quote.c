cdcfcae46a35803417188038936c0fdf56748cea
pipeleon <oflg94@gmail.com>
2021-09-30 22:53:44 -0700

    Task 101 v7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	static const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(str, sizeof(str) - 1, 1, stdout);
 	/* return an int */
