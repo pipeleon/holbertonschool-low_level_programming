1e50aa51231333235d194f65d239c0a61fbcc492
pipeleon <oflg94@gmail.com>
2021-10-01 13:00:10 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* Task 5 0x01 Variables, if, else, while  */
+
+/**
+ * main - Print numbers
+ * Description: Print numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	printf("\n");
+
+	return (0);
+}
