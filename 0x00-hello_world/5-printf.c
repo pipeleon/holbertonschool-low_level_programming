4428ad69dc5b3ffe28c293ccbabe706b030c01a4
pipeleon <oflg94@gmail.com>
2021-09-30 21:01:42 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* Task 5 0 Low Programming */
+
+/**
+ * main - Output with function printf
+ * Description: Just print something
+ * Return: 0
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        /* return an int */
+        return (0);
+}
