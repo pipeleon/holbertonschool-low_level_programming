9f883ac8772d4277d50604f2dab7161e3af15f9d
pipeleon <oflg94@gmail.com>
2021-10-01 13:11:14 -0700

    Task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* Task 8 0x01 Variables, if, else, while  */
+
+/**
+ * main - Print number base 16
+ * Description: Print numbers base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+
+	for (b = 97; b <= 102; b++)
+		putchar(b);
+
+	putchar('\n');
+
+	return (0);
+}
