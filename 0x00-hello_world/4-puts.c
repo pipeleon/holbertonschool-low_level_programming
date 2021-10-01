9842865e570023abad0f3b538e8c2c17c962cbae
pipeleon <oflg94@gmail.com>
2021-09-30 20:51:20 -0700

    Task 4 v5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/* Task 4 0 Low Programming */
+
 /**
- * Output with function puts
+ * main - Output with function puts
+ * Description: Just print something
+ * Return: 0
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
+	/* return an int */
 	return (0);
 }
