a94047aed5aad3a30d10ecc98778c58ffd17a3ff
pipeleon <oflg94@gmail.com>
2021-10-01 14:05:57 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/* Task 9 0x01 Variables, if, else, while  */
+
+/**
+ * main - Print numbers
+ * Description: Print numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	c = 0;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 49 + c; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (b != 57 && a != 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		c++
+
+	}
+	putchar('\n');
+
+	return (0);
+}
