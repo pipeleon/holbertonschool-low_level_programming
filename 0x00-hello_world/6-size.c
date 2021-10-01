398593dda5f6c8fda4c0e78c76827df11b6e1960
pipeleon <oflg94@gmail.com>
2021-09-30 21:43:27 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* Task 6 0 Low Programming */
+
+/**
+ * main - Output sizes of types
+ * Description: Just print sizes of types
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	/* return an int */
+	return (0);
+}
