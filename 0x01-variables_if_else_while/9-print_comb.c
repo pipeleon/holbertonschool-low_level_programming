9578a4b22c0064f2bd7092a93d98cce095cd4868
pipeleon <oflg94@gmail.com>
2021-10-01 13:17:52 -0700

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 {
 	int a;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a <= 56; a++)
 	{
 		putchar(a);
 		putchar(44);
 		putchar(32);
 	}
+	putchar(57);
 	putchar('\n');
 
 	return (0);
