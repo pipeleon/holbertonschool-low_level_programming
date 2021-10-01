8e788d2a1912e4be56de44d01680b424769cf8e4
pipeleon <oflg94@gmail.com>
2021-09-30 22:01:44 -0700

    Task 100

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* Task 4 0 Low Programming */
+
+/**
+ * main - Output with function puts
+ * Description: Just print something
+ * Return: 0
+ */
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	/* return an int */
+	return (1);
+}
