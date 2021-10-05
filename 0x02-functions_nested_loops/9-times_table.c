1e838d6d6a55ec82bcd7b5fce5efc301db267f12
pipeleon <oflg94@gmail.com>
2021-10-05 16:49:45 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/* Task 9 0x02. C - Functions, nested loops  */
+
+/**
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int n;
+
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			n = (b * a);
+			if ((n / 10) == 0)
+				_putchar(' ');
+			else
+				_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
+			if (a != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
