8ef4229a28e6b414e2bf25d46d6029c54f7a3b52
pipeleon <oflg94@gmail.com>
2021-10-04 18:21:08 -0700

    Task 102 final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,52 +14,37 @@ int main(void)
 	e = 0;
 	f = 0;
 	g = 0;
-	h = 1;
 	cont = 0;
 	i = 0;
 
-	for (a = 48; a <= 57; a++)
-	{
+	for (a = 48; a <= 57; a++){
 		if (a == 57)
 			g = 1;
 		h = 1;
-		if (a != 48)
-		{
+		if (a != 48){
 			i++;
-			f = 0;
-		}
-		for (b = 48; b <= 57 - g; b++)
-		{
-			if (b == 57)
-			{
+			f = 0;}
+		for (b = 48; b <= 57 - g; b++){
+			if (b == 57){
 				f++;
 				e = 0;
-				cont = 0;
-			}
-			else
-			{
-				if (b != 48)
-				{
+				cont = 0;}
+			else{
+				if (b != 48){
 					cont++;
 					e = cont;
-					h = 1;
-				}
+					h = 1;}
 			}
-			for (c = 48 + f + i; c <= 57; c++)
-			{
-				for (d = 48 + e + h; d <= 57; d++)
-				{
+			for (c = 48 + f + i; c <= 57; c++){
+				for (d = 48 + e + h; d <= 57; d++){
 					putchar(a);
 					putchar(b);
 					putchar(32);
 					putchar(c);
 					putchar(d);
-					if (a != 57 || b != 56 || c != 57 || d != 57)
-					{
+					if (a != 57 || b != 56 || c != 57 || d != 57){
 						putchar(44);
-						putchar(32);
-					}
-
+						putchar(32);}
 				}
 				h = 0;
 				e = 0;
