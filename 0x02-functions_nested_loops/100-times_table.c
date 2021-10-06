0e122e1e2a5b8a9814f657777dfa9b65ab64ba46
pipeleon <oflg94@gmail.com>
2021-10-05 18:23:10 -0700

    Task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/* Task 100 0x02. C - Functions, nested loops  */
+
+/**
+ * print_times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			for (a = 0; a <= n; a++)
+			{
+				c = (b * a);
+				if (a == 0)
+					_putchar('0');
+				else if ((c / 100) == 0 && a != 0)
+				{
+					_putchar(' ');
+					if ((c / 10) == 0)
+						_putchar(' ');
+					else
+						_putchar('0' + (c / 10));
+				}
+				else
+				{
+					_putchar('0' + (c / 100));
+					_putchar('0' + ((c % 100) / 10));
+				}
+				if (a != 0)
+					_putchar('0' + (c % 10));
+				if (a != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
