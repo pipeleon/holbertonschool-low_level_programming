e60fb249903d7fbc2b90891ce4432a9521b8a58b
pipeleon <oflg94@gmail.com>
2021-10-05 17:12:48 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* Task 10 0x02. C - Functions, nested loops  */
+
+/**
+ * add - Add the parameters
+ * @a: First number
+ * @b: Second number
+ * Description: Add two numbers given
+ * Return: int
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+
+	return (c);
+}
