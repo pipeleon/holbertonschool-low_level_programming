d2b062a602f7ee07aabf27a1dd6a83bb725155bb
pipeleon <oflg94@gmail.com>
2021-10-05 20:28:52 -0700

    Task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/* Task 103 0x02. C - Functions, nested loops  */
+
+/**
+ * main -  prints the sume of the even fibonacci
+ * Description:  prints the sume of the even Fibonacci numbers
+ * Return: void
+ */
+int main(void)
+{
+	long a;
+	long b;
+	long c;
+	long suma;
+
+	b = 1;
+
+	while (b <= 4000000)
+	{
+		if (b == 1)
+		{
+			a = 1;
+			b = 2;
+		}
+		else
+		{
+			if ((b % 2) == 0)
+				suma = (suma + b);
+			c = b;
+			b = (c + a);
+			a = c;
+		}
+	}
+	printf("%lu", suma);
+	putchar('\n');
+	return (0);
+}
