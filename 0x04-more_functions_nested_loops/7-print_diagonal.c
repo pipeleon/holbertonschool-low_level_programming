a145491eb83cece8b1955e219c369095d7e55883
pipeleon <oflg94@gmail.com>
2021-10-08 16:07:28 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/* Task 7 0x04. C - More functions, more nested loops  */
+
+/**
+ * print_diagonal - Prints a diagonal
+ * @n: number of diagonals
+ * Description: Prints diagonal
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		_putchar(92);
+		_putchar('\n');
+
+		if (n > 1)
+		{
+			for (i = 2; i <= n; i++)
+			{
+				for (j = 1; j <= (i - 1); j++)
+					_putchar(32);
+				_putchar(92);
+				_putchar('\n');
+			}
+		}
+	}
+	else
+		_putchar('\n'); 
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c~
@@ -0,0 +1,20 @@
+#include "main.h"
+/* Task 6 0x04. C - More functions, more nested loops  */
+
+/**
+ * print_line - Prints lines
+ * @n: number of lines
+ * Description: Prints lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		_putchar(95);
+	}
+	_putchar('\n');
+}
