765e8d1be35063261b32a43096a3b5162ac02900
pipeleon <oflg94@gmail.com>
2021-10-08 19:14:06 -0700

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,6 +9,11 @@
  */
 void print_number(int n)
 {
+	if (n < 0)
+	{
+		n = (n * -1);
+		_putchar(45);
+	}
 	if (n == 0)
 		_putchar('0');
 	else if (n > 0 && n < 10)
@@ -31,31 +36,10 @@ void print_number(int n)
 		_putchar('0' + ((n / 10) % 10));
 		_putchar('0' + (n % 10));
 	}
-	else if (n < 0 && n >= -9)
+	else if (n >= 10000 && n < 100000)
 	{
-		_putchar(45);
-		_putchar('0' + (n * -1));
-	}
-	else if (n < -9 && n >= -99)
-	{
-		n = (n * -1);
-		_putchar(45);
-		_putchar('0' + (n / 10));
-		_putchar('0' + (n % 10));
-	}
-	else if (n < -99 && n >= -999)
-	{
-		n = (n * -1);
-		_putchar(45);
-		_putchar('0' + (n / 100));
-		_putchar('0' + ((n / 10) % 10));
-		_putchar('0' + (n % 10));
-	}
-	else if (n < -999 && n >= -9999)
-	{
-		n = (n * -1);
-		_putchar(45);
-		_putchar('0' + (n / 1000));
+		_putchar('0' + (n / 10000));
+		_putchar('0' + ((n / 1000) % 10));
 		_putchar('0' + ((n / 100) % 10));
 		_putchar('0' + ((n / 10) % 10));
 		_putchar('0' + (n % 10));
