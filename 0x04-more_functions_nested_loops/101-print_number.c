c2d6047550dba32f3c7f9c5eb5a3425cef7938d2
pipeleon <oflg94@gmail.com>
2021-10-08 19:16:32 -0700

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -44,4 +44,13 @@ void print_number(int n)
 		_putchar('0' + ((n / 10) % 10));
 		_putchar('0' + (n % 10));
 	}
+	else if (n >= 100000 && n < 1000000)
+	{
+		_putchar('0' + (n / 100000));
+		_putchar('0' + ((n / 10000) % 10));
+		_putchar('0' + ((n / 1000) % 10));
+		_putchar('0' + ((n / 100) % 10));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
 }
