1fc3e8e8aae2727d6f16bc2f9c76ab824372799a
pipeleon <oflg94@gmail.com>
2021-10-11 22:15:41 -0700

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 	int i;
 	int j;
 	int l;
-	int a;
+	unsigned int a;
 	int cont;
 
 	i = 0;
@@ -29,13 +29,22 @@ int _atoi(char *s)
 	for (j = 0 + cont; j <= l - 1; j++)
 	{
 		if (s[j] >= 48 && s[j] <= 57)
-			a = (a*10 + s[j] - '0');
+		{
+			a = ((a * 10) + s[j] - '0');
+			if (s[j + 1] <= 48 || s[j + 1] >= 57)
+				break;
+		}
 	}
 
-	for (j = 0; j <= l -1; j++)
+	cont = 0;
+	j = 0;
+	while (cont != 1)
 	{
+		if (s[j] >= 48 && s[j] <= 57)
+			cont = 1;
 		if (s[j] == 45)
 			a = (a * -1);
+		j++;
 	}
 	return (a);
 }
