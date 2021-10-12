2c04c69305f788ee16a844f08b754444ba6fb780
pipeleon <oflg94@gmail.com>
2021-10-11 19:12:25 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* Task 0 0x05. C - Pointers, arrays and strings  */
+
+/**
+ * reset_to_98 - Change a int to 98
+ * @n: Number to reset
+ * Description: Reset to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int m;
+	int *p;
+
+	p = &m;
+
+	*p = n;
+}
