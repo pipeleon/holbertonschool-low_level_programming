add99857934584572d9f05cbfc9a07a5d30573c6
pipeleon <oflg94@gmail.com>
2021-10-11 19:48:02 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* Task 3 0x05. C - Pointers, arrays and strings  */
+
+/**
+ * _puts - Prinst a string
+ * @str: String to print
+ * Description: Prints a string
+ * Return: void
+ */
+void _puts(char *str);
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
