4da88ead298703402589833adc7b2bb154c83bdd
pipeleon <oflg94@gmail.com>
2021-10-12 17:10:34 -0700

    Task 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "main.h"
+/* Task 101 0x05. C - Pointers, arrays and strings  */
+
+/**
+ * main - Generates a password
+ * Description: Generates a password
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+	int random;
+	char n[];
+	char lo[];
+	char up[];
+	char password[8];
+
+	srand((unsigned int)(time(NULL)));
+
+	random = 0;
+	n[] = "0123456789";
+	lo[] = "abcdefghijklmnoqprstuvwyzx";
+	up[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+	random = rand() % 4;
+
+	for (i = 0; i < 8; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = n[rand() % 10];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = lo[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = up[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = lo[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+	return (0);
+}
