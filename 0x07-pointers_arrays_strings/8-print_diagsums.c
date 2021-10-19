87c406de0794d75d7562734e65ad6c9aed48d754
pipeleon <oflg94@gmail.com>
2021-10-18 21:54:26 -0700

    Task 7 Test 2 right betty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,10 +3,10 @@
 /* Task 7 0x07. C - Even more pointers, arrays and strings  */
 
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix of integers
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix
  * @a: Matrix
  * @size: Size of matrix
- * Description: Prints the sum of the two diagonals of a square matrix of integers
+ * Description: Prints the sum of the two diagonals of a square matrix
  * Return: void
  */
 void print_diagsums(int *a, int size)
