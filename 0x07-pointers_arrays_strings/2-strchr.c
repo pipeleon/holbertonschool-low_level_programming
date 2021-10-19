6c57f6f81ec7f21486e618a31a4581bfc4a5d93e
pipeleon <oflg94@gmail.com>
2021-10-18 19:04:41 -0700

    Task 2 Test 1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/* Task 2 0x07. C - Even more pointers, arrays and strings  */
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String to search
+ * @c: Character
+ * Description: Locates a character in a string
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *p;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			break;
+		i++;
+	}
+
+	if (s[i] == '\0')
+		p = 0;
+	else
+		p = &s[i];
+	
+	return (p);
+}
