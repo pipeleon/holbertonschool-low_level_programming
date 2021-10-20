7a7525eac73a90adb664af92451ba74ac6074c63
pipeleon <oflg94@gmail.com>
2021-10-20 11:18:29 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,17 @@
  * Description: Prints a string
  * Return: void
  */
+char uchar(char *s, int i)
+{
+	return s[i];
+}
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+	{
+		_putchar('\n');
 		return;
+	}
 	_putchar(*s);
-	_puts_recursion(*s + 1);
+	_puts_recursion(s + 1);
 }
