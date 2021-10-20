5bbf0512169aa657ad1eeaaf6fcabe03b6e8e0ef
pipeleon <oflg94@gmail.com>
2021-10-20 14:12:56 -0700

    Task 1 Test 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,22 +7,11 @@
  * Description: Prints a string in reverse
  * Return: void
  */
-int len(char *s)
-{
-	int l;
-
-	l = 0;
-	while (s[l] != '\0')
-		l++;
-	return (l);
-}
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*(s + len(s) - 1));
-	_print_rev_recursion(s + 1);
 }
