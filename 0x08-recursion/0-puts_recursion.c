23a7dfbd9fc4b0b93b42c6a4436a1ae9fa540264
pipeleon <oflg94@gmail.com>
2021-10-20 10:35:46 -0700

    Adding README.md, main.h and Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* Task 0 0x08. C - Recursion  */
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: String
+ * Description: Prints a string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(*s + 1);
+}
