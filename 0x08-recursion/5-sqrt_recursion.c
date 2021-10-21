62cb2c91a6777904f4e3eb897d25796c209b2fd5
pipeleon <oflg94@gmail.com>
2021-10-20 17:51:03 -0700

    Task 5 Test 1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/* Task 5 0x08. C - Recursion  */
+
+int _sqrt(int a, int n)
+{
+	int m;
+
+	m = a;
+
+	if (m * m <= n && (m + 1) * (m + 1) > n)
+		return m;
+	else
+		return _sqrt((m + 1), n);
+}
+
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: Number
+ * Description: Returns the natural square root of a number
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int sq;
+
+	if (n == 1)
+		return (1);
+	else
+	{
+		sq = _sqrt(2, n);
+		if (sq * sq == n)
+			return sq;
+		else
+			return (-1);
+	}
+}
