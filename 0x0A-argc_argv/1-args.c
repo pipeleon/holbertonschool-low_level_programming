6e4275123001ff21ace8f5b65a74f4581a882464
pipeleon <oflg94@gmail.com>
2021-10-22 15:42:03 -0700

    Task 0 Test 2 betty style ok

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Description: Prints the number of arguments passed into it
  * Return: int
  */
-int main(int argc , char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", (argc - 1));
 	return (0);
