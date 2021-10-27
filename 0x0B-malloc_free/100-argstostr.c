75c7a02992d8ad6d0cf3691dfcc8e54cdd8ba153
pipeleon <oflg94@gmail.com>
2021-10-27 10:59:12 -0700

    Task 100 test 2 adding condacate function

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,6 +4,53 @@
 /* Task 100 0x0B. C - malloc, free  */
 
 /**
+ * str_concat -  concatenates two strings
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	int la, lb;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	la = 0;
+	while (s1[la] != '\0')
+		la++;
+	lb = 0;
+	while (s2[lb] != '\0')
+		lb++;
+
+	s = malloc((la + lb + 1) * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < la)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < lb)
+	{
+		s[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	return (s);
+}
+
+/**
  * argstostr - concatenates all the arguments of your program
  * @ac: number of arguments
  * @av: array of arguments
@@ -12,22 +59,16 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
 	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	s = malloc(ac * sizeof(char));
-
-	if (s == NULL)
-		return (NULL);
-
-	i = 0;
-	while (i < ac)
+	s = "";
+	while (ac--)
 	{
-		*(s + i) = **(av + i);
-		i++;
+		s = str_concat(s, *av++);
+		s = str_concat(s, "\n");
 	}
 
 	return (s);
