a4a324ca626bcba396387923e6ff4f514751f9c0
pipeleon <oflg94@gmail.com>
2021-11-03 10:56:07 -0700

    Task 1 corrected header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
+#include <stddef.h>
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
 void print_name(char *name, void (*f)(char *));
-
-typedef int size_t;
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 
