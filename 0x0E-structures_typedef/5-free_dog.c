4f2b80a4a69640341598723ab57938ba4dd2f1ac
pipeleon <oflg94@gmail.com>
2021-11-02 21:10:22 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* Task 5 0x0E. C - Structures, typedef  */
+
+/**
+ * free_dog -  free dog struct
+ * @d: Dog struct
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
