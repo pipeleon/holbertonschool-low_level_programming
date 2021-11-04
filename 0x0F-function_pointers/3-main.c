5e0f92af265810ddb2a4c551620e7d434a7b1bbd
pipeleon <oflg94@gmail.com>
2021-11-04 16:49:17 -0700

    task 3 test corrected error 99

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	int res;
 	char *o;
 	int num;
-	int a;
+	int a, le;
 
 	a = argc;
 	if (a != 4)
@@ -26,8 +26,16 @@ int main(int argc, char *argv[])
 
 	o = argv[2];
 	num = atoi(argv[3]);
+	le = 0;
+	while (o[le] != '\0')
+		le++;
 
 	if ((*o != '+' && *o != '-' && *o != '*' && *o != '/' && *o != '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (le > 1)
 	{
 		printf("Error\n");
 		exit(99);
