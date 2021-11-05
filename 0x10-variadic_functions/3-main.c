fa656dada5ccffd7b53beecd8b37e38489b010bf
pipeleon <oflg94@gmail.com>
2021-11-05 13:23:18 -0700

    Task 3 test new while for NULL

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	print_all("fcsi", 3.14435, 'H', NULL, 402);
+	print_all(NULL);
 	return (0);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,60 +17,61 @@ void print_all(const char * const format, ...)
 	float f;
 	double d;
 
-
-	va_start(ap, format);
-	len = 0, p = 0;
-	while (format[len] != '\0')
-		len++;
-	while (p < len && format != NULL)
+	len = 0, p = 0, z = 0;
+	while (format != NULL && z == 0)
 	{
-		z = 0;
-		switch (format[p])
+		va_start(ap, format);
+		while (format[p] != '\0')
 		{
-		case 'i':
-		{
-			i = va_arg(ap, int), printf("%d", i);
-			break;
-		}
-		case 'f':
-		{
-			f = va_arg(ap, double);
-			printf("%f", f);
-			break;
-		}
-		case 'c':
-		{
-			c = va_arg(ap, int);
-			printf("%c", c);
-			break;
-		}
-		case 'd':
-		{
-			/*if (va_arg(ap, double) == '\0')
+			z = 0;
+			switch (format[p])
+			{
+			case 'i':
+			{
+				i = va_arg(ap, int), printf("%d", i);
+				break;
+			}
+			case 'f':
+			{
+				f = va_arg(ap, double);
+				printf("%f", f);
+				break;
+			}
+			case 'c':
+			{
+				c = va_arg(ap, int);
+				printf("%c", c);
+				break;
+			}
+			case 'd':
+			{
+				/*if (va_arg(ap, double) == '\0')
+				  {
+				  z = 1;
+				  break;
+				  }*/
+				d = va_arg(ap, double),	printf("%f", d);
+				break;
+			}
+			case 's':
+			{
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			}
+			default:
 			{
 				z = 1;
 				break;
-				}*/
-			d = va_arg(ap, double),	printf("%f", d);
-			break;
+			}
+			}
+			if (p != len - 1 && z != 1)
+				printf(", ");
+			p++;
 		}
-		case 's':
-		{
-			str = va_arg(ap, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s", str);
-			break;
-		}
-		default:
-		{
-			z = 1;
-			break;
-		}
-		}
-		if (p != len - 1 && z != 1)
-			printf(", ");
-		p++;
+		z = 1;
 	}
 
 	printf("\n");
