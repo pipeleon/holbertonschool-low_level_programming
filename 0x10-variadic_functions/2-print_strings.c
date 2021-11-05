341d93eaea59cd81bfa70bf2caf8aacd503cf004
pipeleon <oflg94@gmail.com>
2021-11-05 08:42:57 -0700

    Task 2 Test with null case and n = 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,20 +22,33 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	for (i = 0; i < n - 1; i++)
+	if (n == 0)
+		printf("\n");
+	else
 	{
+		for (i = 0; i < n - 1; i++)
+		{
+			s = va_arg(ap, char*);
+			if (separator == NULL)
+			{
+				if (s != NULL)
+					printf("%s", s);
+				else
+					printf("(nil)");
+			}
+			else
+			{
+				if (s != NULL)
+					printf("%s%s", s, separator);
+				else
+					printf("(nil)%s", separator);
+			}
+		}
 		s = va_arg(ap, char*);
 		if (s != NULL)
-			printf("%s%s", s, separator);
+			printf("%s\n", s);
 		else
-			printf("(nil)%s", separator);
+			printf("(nil)\n");
 	}
-
-	s = va_arg(ap, char*);
-	if (s != NULL)
-		printf("%s\n", s);
-	else
-		printf("(nil)\n");
-
 	va_end(ap);
 }
