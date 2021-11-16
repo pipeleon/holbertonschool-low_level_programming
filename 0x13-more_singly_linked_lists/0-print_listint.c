46de7e1ff952eaaea63917790d55ff4119d9e32b
pipeleon <oflg94@gmail.com>
2021-11-16 14:34:21 -0800

    Task 0 corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,7 @@ size_t print_listint(const listint_t *h);
 	i = 0;
 	if (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d]\n", h->n);
 		if (h->next != NULL)
 			i += print_list(h->next);
 		i++;
