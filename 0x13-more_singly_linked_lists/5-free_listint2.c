f4579e6a95e237af64ef164daff0cbdd4931023f
pipeleon <oflg94@gmail.com>
2021-11-16 15:44:04 -0800

    task 5 test null at the end

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,20 +2,20 @@
 /* Task 4 0x12. C - Singly linked lists  */
 
 /**
- * free_listint - frees a list_t list
+ * free_listint2 - frees a list_t list
  * @head: Head of the list
  *
  * Return: void
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	if (*head == NULL)
 		return;
 
-	if (head->next != NULL)
-		free_listint(head->next);
-	else
-		head = NULL;
+	if ((*head)->next != NULL)
+		free_listint2(&(*head)->next);
 
-	free(head);
+	free(*head);
+	if ((*head)->next == NULL)
+		head = NULL;
 }
