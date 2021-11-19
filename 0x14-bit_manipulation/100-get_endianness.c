ff292f284434b0c0056e1d111e91617e18450a4c
pipeleon <oflg94@gmail.com>
2021-11-18 22:35:49 -0800

    task 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* Task 100 0x14. C - Bit manipulation  */
+
+/**
+ * get_endianness - checks the endianness.
+ *
+ * Return: int
+ */
+int get_endianness(void)
+{
+	int i = 1;
+
+	if(*(char *)&i == 1)
+		return (1);
+	else
+		return (0);
+}
