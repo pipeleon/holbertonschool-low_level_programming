82fab674bf91fb62d8b7d7c8037f1fdd343b3827
pipeleon <oflg94@gmail.com>
2021-11-22 17:57:10 -0800

    Task 3 set mode 664

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -37,6 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	write(STDOUT_FILENO, buf, cont);
 
 	close(fd);
-	
+	free(buf);
+
 	return (cont);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,33 @@
 #include "main.h"
+/* Task 4 0x15. C - File I/O  */
+
+/**
+ * len_str - Lenght of a string
+ * @text_content: Content
+ *
+ * Return: int
+ */
+int len_str(char *text_content)
+{
+	int i;
+
+	i = 0;
+	while (*(text_content + i) != '\0')
+		i++;
+
+	return (i);
+}
 
 /**
  * main - copy
+ * @ac: How many arguments
+ * @av: Array of arguments
  *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-	int fd, fd2, cl;
-	int cont;
+	int fd, fd2, cl, cont;
 	char bf[1024];
 
 	if (ac != 3)
@@ -16,44 +35,32 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd = open(av[1], O_RDONLY);
-
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
-	cont = 0;
-	while (av[1][cont] != '\0')
-		cont++;
-
 	read(fd, bf, 1024);
-
 	cl = close(fd);
 	if (cl == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-
-	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
+	cont = len_str(bf);
 	write(fd2, bf, cont);
-
 	cl = close(fd2);
 	if (cl == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
-
 	return (0);
 }
