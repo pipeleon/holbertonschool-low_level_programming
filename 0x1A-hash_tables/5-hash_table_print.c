51d585423e8f91488196a7509fb592a32c20baa7
pipeleon <oflg94@gmail.com>
2022-01-28 00:33:35 -0800

    Task 5 betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,29 +4,29 @@
 /**
  * hash_table_print -  prints a hash table
  * @ht: Hash table
- * 
+ *
  * Return: Void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *tmp;
-    int flag = 0;
+	unsigned long int i;
+	hash_node_t *tmp;
+	int flag = 0;
 
-    if (ht == NULL)
-        return;
-    
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        while (tmp)
-        {
-            if (flag != 0)
-                printf(", ");
-            printf("'%s': '%s'", tmp->key, tmp->value);
-            tmp = tmp->next, flag = 1;
-        }
-    }
-    printf("}\n");
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			if (flag != 0)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next, flag = 1;
+		}
+	}
+	printf("}\n");
 }
