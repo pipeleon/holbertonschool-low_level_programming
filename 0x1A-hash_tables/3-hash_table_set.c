a594df48663385652f85fad5cf51ba5a3613f61a
pipeleon <oflg94@gmail.com>
2022-01-28 02:47:46 -0500

    task 5

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -58,7 +58,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     }
     
     index = key_index((unsigned char *)key, ht->size);
-    
     if (ht->array[index] == NULL)
     {        
         new->key = strdup(key);     
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/* Task 5 0x1A. C - Hash tables */
+
+/**
+ * hash_table_print -  prints a hash table
+ * @ht: Hash table
+ * 
+ * Return: Void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *tmp;
+    int flag = 0;
+
+    if (ht == NULL)
+        return;
+    
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        tmp = ht->array[i];
+        while (tmp)
+        {
+            if (flag != 0)
+                printf(", ");
+            printf("'%s': '%s'", tmp->key, tmp->value);
+            tmp = tmp->next, flag = 1;
+        }
+    }
+    printf("}\n");
+}
