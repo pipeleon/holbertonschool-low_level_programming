806bcb3e0eb12ce90e38cebef69d345b99ec42ff
pipeleon <oflg94@gmail.com>
2022-01-28 01:29:59 -0500

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     unsigned long int index;
     hash_node_t *new;    
 
-    if (ht == NULL)
+    if (ht == NULL || key == NULL || value == NULL)
     {
         return (0);
     }
