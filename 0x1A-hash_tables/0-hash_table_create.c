d61060bcb6268b4d32652d2b0194e869ac0b0f67
pipeleon <oflg94@gmail.com>
2022-01-28 03:02:16 -0500

    All

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,26 +9,34 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new;
-    unsigned long int i;
+  hash_table_t *new;
+  unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
+  if (size == 0)
+  {
+    return (NULL);
+  }
+        
     
-    new = malloc(sizeof(hash_table_t));
-    if (new == NULL)
-		return (NULL);
+  new = malloc(sizeof(hash_table_t));
+  if (new == NULL)
+	{
+    return (NULL);
+  }
 
-    new->size = size;
+  new->size = size;
 
-    new->array = malloc(size * sizeof(hash_node_t*));
-     if (new->array == NULL)
-		return (NULL);
-    
-    for (i = 0; i < size; i++)
-    {
-        new->array[i] = NULL;
-    }
+  new->array = malloc(size * sizeof(hash_node_t*));
 
-    return (new);
+  if (new->array == NULL)
+	{
+    return (NULL);
+  }	
+
+  for (i = 0; i < size; i++)
+  {
+    new->array[i] = NULL;
+  }
+
+  return (new);
 }
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/* Task 6 0x1A. C - Hash tables */
+
+/**
+ * hash_table_delete -  delete a hash table
+ * @ht: Hash table
+ * 
+ * Return: Void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *tmp;
+
+    if (ht == NULL)
+        return;
+    
+    for (i = 0; i < ht->size; i++)
+    {
+        tmp = ht->array[i];
+        while (tmp)
+        {
+            free(tmp->key);
+            free(tmp->value);
+            free(tmp);
+            tmp = tmp->next;
+        }
+    }
+    free(ht);
+}
