82f22e1be3b7a088518d2f4edd61c1bf5db81b1e
pipeleon <oflg94@gmail.com>
2022-01-28 00:09:13 -0800

    Task 0 betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,34 +9,33 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new;
-  unsigned long int i;
+	hash_table_t *new;
+	unsigned long int i;
 
-  if (size == 0)
-  {
-    return (NULL);
-  }
-        
-    
-  new = malloc(sizeof(hash_table_t));
-  if (new == NULL)
+	if (size == 0)
 	{
-    return (NULL);
-  }
+		return (NULL);
+	}
 
-  new->size = size;
-
-  new->array = malloc(size * sizeof(hash_node_t*));
-
-  if (new->array == NULL)
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
 	{
-    return (NULL);
-  }	
+		return (NULL);
+	}
 
-  for (i = 0; i < size; i++)
-  {
-    new->array[i] = NULL;
-  }
+	new->size = size;
 
-  return (new);
+	new->array = malloc(size * sizeof(hash_node_t *));
+
+	if (new->array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		new->array[i] = NULL;
+	}
+
+	return (new);
 }
