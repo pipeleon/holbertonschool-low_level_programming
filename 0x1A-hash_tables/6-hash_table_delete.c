8d0798d137121f4bddbce65f80e7cd05f6b79f62
pipeleon <oflg94@gmail.com>
2022-01-28 00:36:42 -0800

    Task 6 betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,27 +4,27 @@
 /**
  * hash_table_delete -  delete a hash table
  * @ht: Hash table
- * 
+ *
  * Return: Void
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *tmp;
+	unsigned long int i;
+	hash_node_t *tmp;
 
-    if (ht == NULL)
-        return;
-    
-    for (i = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        while (tmp)
-        {
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-            tmp = tmp->next;
-        }
-    }
-    free(ht);
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = tmp->next;
+		}
+	}
+	free(ht);
 }
