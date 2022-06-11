9f73eca66e4f9f2ec7c75f180c974f66ff641428
pipeleon <oflg94@gmail.com>
2022-06-11 16:09:41 -0500

    Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,74 @@
 #include "search_algos.h"
-/* Task 0 0x1E. C - Search Algorithms */
+/* Task 1 0x1E. C - Search Algorithms */
+
+/**
+ * binary_search2 - searches for a value
+ * @array: pointer to the first element of the array
+ * @a: begin search
+ * @b: end search
+ * @value: Value to search
+ *
+ * Return: index for the value search
+ */
+int binary_search2(int *array, size_t a, size_t b, int value)
+{
+    size_t i, size, middle;
+
+    printf("Searching in array: ");
+    for (i = a; i < b; i++)
+    {
+        printf("%d", array[i]);
+        if (i < b - 1)
+        {
+            printf(", ");
+        }
+    }
+    printf("\n");
+
+    size = b - a;
+
+    if (size <= 0)
+    {
+        return (-1);
+    }
+
+    if (size % 2 == 0)
+    {
+        middle = size / 2;
+        if (array[middle] == value)
+        {
+            return (middle);
+        }
+        else if (array[middle] > value)
+        {
+            return (binary_search2(array, a, array[middle - 1], value));
+        }
+        else if (array[middle + 1] == value)
+        {
+            return (middle + 1);
+        }
+        else
+        {
+            return (binary_search2(array, array[middle + 2], b, value));
+        }
+    }
+    else
+    {
+        middle = (size + 1) / 2;
+        if (array[middle] == value)
+        {
+            return (middle);
+        }
+        else if (array[middle] > value)
+        {
+            return (binary_search2(array, a, array[middle - 1], value));
+        }
+        else
+        {
+            return (binary_search2(array, array[middle + 1], b, value));
+        }
+    }
+}
 
 /**
  * binary_search - searches for a value
@@ -11,11 +80,11 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int i;
-
     if (array == NULL)
     {
         return (-1);
     }
+
+    return binary_search2(array, 0, size, value);
     
 }
