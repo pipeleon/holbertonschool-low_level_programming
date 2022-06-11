5b61282082d5b6c337929b2abcfac2197016609d
pipeleon <oflg94@gmail.com>
2022-06-11 15:15:01 -0500

    Task 0 and README

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+/* Task 0 0x1E. C - Search Algorithms */
+
+/**
+ * binary_search - searches for a value
+ * @array: pointer to the first element of the array
+ * @size: Size of the array
+ * @value: Value to search
+ *
+ * Return: index for the value search
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    int i;
+
+    if (array == NULL)
+    {
+        return (-1);
+    }
+    
+}
